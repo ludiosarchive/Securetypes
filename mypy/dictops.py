f1756638faff21501205600f73710693a03a5cfe
ivank <ivank@ludios.net>
2010-05-11 14:59:24 +0000

    Add dictops.frozendict and tests

---
+++ mypy/dictops.py
@@ -0,0 +1,44 @@
+class frozendict(dict):
+	"""
+	A C{dict} that tries hard to be immutable.
+	"""
+	__slots__ = ('_cachedHash')
+
+	@property
+	def _blocked(obj):
+		raise AttributeError("A frozendict cannot be modified.")
+
+	__delitem__ = \
+	__setitem__ = \
+	clear = \
+	pop = \
+	popitem = \
+	setdefault = \
+	update = \
+	_blocked
+
+	# TODO: make sure that viewitems, viewkeys, viewvalues do not
+	# allow mutation of the dict.
+
+	def __new__(cls, *args, **kwargs):
+		new = dict.__new__(cls)
+		new._cachedHash = None
+		dict.__init__(new, *args, **kwargs)
+		return new
+
+
+	# A Python dict can be updated with __init__ after it is created,
+	# which is the only reason we override __init__ and __new__.
+	def __init__(self, *args, **kwargs):
+		pass
+
+
+	def __hash__(self):
+		h = self._cachedHash
+		if h is None:
+			h = self._cachedHash = hash(tuple(self.iteritems()))
+		return h
+
+
+	def __repr__(self):
+		return "frozendict(%s)" % dict.__repr__(self)
