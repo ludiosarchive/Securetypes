26d7fe715fc60724862d52cde49d956c9418d8d7
Ivan Kozik <ivan@ludios.org>
2011-05-05 10:25:49 +0000

    setup.py: Add author/url/classifiers

--- setup.py
+++ setup.py
@@ -9,5 +9,15 @@ setup(
 	version=securedict.__version__,
 	description="Implementation of dict that protects against " +
 		"algorithmic complexity attacks",
+	url="https://github.com/ludios/Securedict",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 4 - Beta',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
 	py_modules=['securedict', 'test_securedict'],
 )
